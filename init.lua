8fd31ae14ae0cee6cd3413d93a6d71b98f13cc52
ionutrogojan <i.rogojan15@gmail.com>
2024-02-03 22:26:54 +0000

    nvim system clipboard

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = true	-- relative numbers to the active line
 vim.opt.tabstop = 4 			-- indentation size
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
--- vim.opt.expandtab = true     -- expand \t to spaces
+vim.opt.expandtab = true     -- expand \t to spaces
 
 vim.opt.smartindent = true		-- indent relative to the above line
 
@@ -19,6 +19,8 @@ vim.opt.updatetime = 50         -- update time for plugins in ms
 vim.opt.background = 'dark'		-- dark mode
 -- vim.cmd('colorscheme torte')	-- builtin theme
 vim.cmd([[colorscheme gruvbox]])
+
+vim.opt.clipboard:append('unnamedplus')    -- use system clipboard
 
 vim.api.nvim_set_keymap('n', 'W', 'b', {noremap = true})	-- remap 'W' to beginning back word
 vim.api.nvim_set_keymap('n', 'E', 'ge', {noremap = true})	-- remap 'E' to end back word
